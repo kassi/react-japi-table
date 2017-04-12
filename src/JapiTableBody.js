12ddefc51d400bbaff4836b1405779bf543fcca0
Karsten Silkenbäumer <ksi@kluks.de>
2017-04-12 23:23:06 +0200

    Add data rows to body

--- src/JapiTableBody.js
+++ src/JapiTableBody.js
@@ -3,7 +3,13 @@ import React from 'react';
 export default class JapiTableBody extends React.Component {
   render () {
     return (
-      <tbody />
+      <tbody>
+        {this.props.data && this.props.data['data'] ? this.props.data['data'].map((row, i) => {
+          return (
+            <tr key={i} />
+          );
+        }) : undefined}
+      </tbody>
     );
   }
 }
--- tests/JapiTableBody.test.js
+++ tests/JapiTableBody.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
 
 import JapiTableBody from '../src/JapiTableBody';
@@ -6,5 +7,26 @@ import JapiTableBody from '../src/JapiTableBody';
 describe('JapiTableHead', () => {
   it('renders a tbody', () => {
     expect(shallow(<JapiTableBody />).type()).toBe('tbody');
+  });
+
+  it('renders a table row for each data entry', () => {
+    const columns = [
+      { header: 'Id' },
+      { header: 'Title' }
+    ];
+    const data = {
+      data: [
+        {
+          id: '1',
+          type: '_type'
+        },
+        {
+          id: '2',
+          type: '_type'
+        }
+      ]
+    };
+    const node = renderer.create(<JapiTableBody columns={columns} data={data} />);
+    expect(node).toMatchSnapshot();
   });
 });
