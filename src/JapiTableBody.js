a74a1df3c0fc8e2e516db736025ff7ef527cf100
Karsten Silkenbäumer <ksi@kluks.de>
2017-04-13 09:31:49 +0200

    Refactor body component by extracting value function

--- src/JapiTableBody.js
+++ src/JapiTableBody.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
 export default class JapiTableBody extends React.Component {
+  getValue (row, column) {
+    const key = column.key;
+    let value = key === 'id' ? row['id'] : (typeof row['attributes'] !== 'undefined' ? row['attributes'][key] : undefined);
+    if (column.renderValue) {
+      value = column.renderValue(value);
+    }
+    return value;
+  }
+
   render () {
     return (
       <tbody>
@@ -8,14 +17,9 @@ export default class JapiTableBody extends React.Component {
           return (
             <tr key={i}>
               {this.props.columns.map((column, j) => {
-                const key = column.key;
-                let value = key === 'id' ? row['id'] : (typeof row['attributes'] !== 'undefined' ? row['attributes'][key] : undefined);
-                if (column.renderValue) {
-                  value = column.renderValue(value);
-                }
                 return (
                   <td className={column['cellClassName']} key={'' + i + '-' + j}>
-                    {value}
+                    {this.getValue(row, column)}
                   </td>
                 );
               })}
